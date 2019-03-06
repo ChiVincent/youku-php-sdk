d52e3a556102d700f8d9db6f04bdfea3cd6d8031
Vincent Chi <song374561@chivincent.net>
2019-03-06 12:29:14 +0800

    Refactoring new slice response, it should extend slice.

--- src/Api/Response/NewSlice.php
+++ src/Api/Response/NewSlice.php
@@ -2,32 +2,12 @@
 
 namespace Chivincent\Youku\Api\Response;
 
-class NewSlice
+class NewSlice extends Slice
 {
-    /**
-     * @var int
-     */
-    private $sliceTaskId;
-
-    /**
-     * @var int
-     */
-    private $offset;
-
-    /**
-     * @var int
-     */
-    private $length;
-
-    /**
-     * @var int
-     */
-    private $transferred;
-
-    /**
-     * @var bool
-     */
-    private $finished;
+    public function __construct(int $sliceTaskId, int $offset, int $length, int $transferred, bool $finished)
+    {
+        parent::__construct($sliceTaskId, $offset, $length, $transferred, $finished);
+    }
 
     public static function json(string $json): ?NewSlice
     {
@@ -50,54 +30,5 @@ class NewSlice
             $response->transferred,
             $response->finished,
         );
-    }
-
-    public function __construct(int $sliceTaskId, int $offset, int $length, int $transferred, bool $finished)
-    {
-        $this->sliceTaskId = $sliceTaskId;
-        $this->offset = $offset;
-        $this->length = $length;
-        $this->transferred = $transferred;
-        $this->finished = $finished;
-    }
-
-    /**
-     * @return int
-     */
-    public function getSliceTaskId(): int
-    {
-        return $this->sliceTaskId;
-    }
-
-    /**
-     * @return int
-     */
-    public function getOffset(): int
-    {
-        return $this->offset;
-    }
-
-    /**
-     * @return int
-     */
-    public function getLength(): int
-    {
-        return $this->length;
-    }
-
-    /**
-     * @return int
-     */
-    public function getTransferred(): int
-    {
-        return $this->transferred;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isFinished(): bool
-    {
-        return $this->finished;
     }
 }
