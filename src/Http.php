4f247deabd30064b84c0427b159616f01b2bb104
Vincent Chi <song374561@chivincent.net>
2019-03-06 10:48:52 +0800

    Refactoring http requests using guzzle.

--- src/Http.php
+++ src/Http.php
@@ -2,64 +2,46 @@
 
 namespace Chivincent\Youku;
 
+use GuzzleHttp\Client;
 use stdClass;
 
 class Http
 {
-    public static function get(string $url, array $params = [])
+    public static function get(string $url, array $params = []): string
     {
-        return self::request('GET', $url, $params);
+        $client = new Client();
+
+        $response = $client->get($url, [
+            'query' => $params,
+        ]);
+
+        return $response->getBody()->getContents();
     }
 
     public static function post(string $url, array $params = [])
     {
-        return self::request('POST', $url, $params);
-    }
+        $client = new Client();
 
-    public static function request(string $method, string $url, array $params = [])
-    {
-        $params = http_build_query($params);
-        $ch = curl_init();
+        $response = $client->post($url, [
+            'form_params' => $params,
+        ]);
 
-        if ($method === 'POST') {
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-        } else {
-            $url .= "?$params";
-        }
-
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        $result = curl_exec($ch);
-
-        curl_close($ch);
-
-        return $result;
+        return $response->getBody()->getContents();
     }
 
     public static function doPostRequest(string $url, array $postData, string $data): stdClass
     {
-        $str = '';
-        foreach ($postData as $key => $value) {
-            $str .= "$key=$value&";
-        }
-        $url = "$url?".rtrim($str, '&');
+        $client = new Client();
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Content-Type: application/octet-stream',
-            'Content-Length: '.strlen($data),
+        $response = $client->post($url, [
+            'headers' => [
+                'Content-Type' => 'application/octet-stream',
+                'Content-Length' => strlen($data),
+            ],
+            'query' => $postData,
+            'body' => $data,
         ]);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $result = curl_exec($ch);
-        curl_close($ch);
 
-        return json_decode($result);
+        return json_decode($response->getBody()->getContents());
     }
 }
