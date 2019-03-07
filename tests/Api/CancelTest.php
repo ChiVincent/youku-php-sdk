9fe684224280115360203b81b39be376b5d6ba04
Vincent Chi <song374561@chivincent.net>
2019-03-07 12:58:59 +0800

    Create test case for Cancel api response

---
+++ tests/Api/CancelTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests;
+
+use Chivincent\Youku\Api\Response\Cancel;
+use PHPUnit\Framework\TestCase;
+
+class CancelTest extends TestCase
+{
+    public function testJson()
+    {
+        $cancel = Cancel::json(json_encode([
+            'upload_token' => '1a2b3c4d',
+        ]));
+
+        $this->assertInstanceOf(Cancel::class, $cancel);
+        $this->assertSame('1a2b3c4d', $cancel->getUploadToken());
+    }
+
+    public function testJsonBuildFailed()
+    {
+        $cancel = Cancel::json(json_encode([
+            'undefined_key' => 'undefined_value',
+        ]));
+
+        $this->assertNull($cancel);
+    }
+
+    public function testConstruct()
+    {
+        $cancel = new Cancel('1a2b3c4d');
+
+        $this->assertInstanceOf(Cancel::class, $cancel);
+        $this->assertSame('1a2b3c4d', $cancel->getUploadToken());
+    }
+
+    public function testGetUploadToken()
+    {
+        $cancel = new Cancel('1a2b3c4d');
+
+        $this->assertSame('1a2b3c4d', $cancel->getUploadToken());
+    }
+}
