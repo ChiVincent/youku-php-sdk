d07230da474d70a19a52fbabaf5518af5fea3a9e
Vincent Chi <song374561@chivincent.net>
2019-04-23 11:36:59 +0800

    Completed sts inf response test case.

--- tests/Api/Response/StsInfTest.php
+++ tests/Api/Response/StsInfTest.php
@@ -26,4 +26,26 @@ class StsInfTest extends TestCase
         $this->assertSame('fake-temp-access-secret', $stsInf->getTempAccessSecret());
         $this->assertSame('2019-01-07T13:03:04Z', $stsInf->getExpireTime());
     }
+
+    public function testJsonBuildFailed()
+    {
+        $stsInf = StsInf::json(json_encode([
+            'undefined_key' => 'undefined_value',
+        ]));
+
+        $this->assertNull($stsInf);
+    }
+
+    public function testConstruct()
+    {
+        $stsInf = new StsInf('1a2b3c4d', 'oss-cn-shanghai.aliyuncs.com', 'fake-security-token', 'fake-temp-access-id', 'fake-temp-access-secret', '2019-01-07T13:03:04Z');
+
+        $this->assertInstanceOf(StsInf::class, $stsInf);
+        $this->assertSame('1a2b3c4d', $stsInf->getUploadToken());
+        $this->assertSame('oss-cn-shanghai.aliyuncs.com', $stsInf->getEndpoint());
+        $this->assertSame('fake-security-token', $stsInf->getSecurityToken());
+        $this->assertSame('fake-temp-access-id', $stsInf->getTempAccessId());
+        $this->assertSame('fake-temp-access-secret', $stsInf->getTempAccessSecret());
+        $this->assertSame('2019-01-07T13:03:04Z', $stsInf->getExpireTime());
+    }
 }
