08fdd4ef68099f1456e39d56843521d523a47cea
Vincent Chi <song374561@chivincent.net>
2019-04-23 10:35:34 +0800

    Build the create api response for oss supporting.

--- src/Api/Response/Create.php
+++ src/Api/Response/Create.php
@@ -17,9 +17,44 @@ class Create extends BaseResponse implements JsonResponse
     private $videoId;
 
     /**
-     * @var string
+     * @var null|string
      */
     private $uploadServerUri;
+
+    /**
+     * @var null|string
+     */
+    private $endpoint;
+
+    /**
+     * @var null|string
+     */
+    private $securityToken;
+
+    /**
+     * @var null|string
+     */
+    private $ossBucket;
+
+    /**
+     * @var null|string
+     */
+    private $ossObject;
+
+    /**
+     * @var null|string
+     */
+    private $tempAccessId;
+
+    /**
+     * @var null|string
+     */
+    private $tempAccessSecret;
+
+    /**
+     * @var null|string
+     */
+    private $expireTime;
 
     /**
      * Make Create Response by json.
@@ -32,7 +67,7 @@ class Create extends BaseResponse implements JsonResponse
         $response = json_decode($json);
 
         $properties = [
-            'upload_token', 'video_id', 'upload_server_uri',
+            'upload_token', 'video_id',
         ];
 
         foreach ($properties as $property) {
@@ -44,15 +79,39 @@ class Create extends BaseResponse implements JsonResponse
         return new self(
             $response->upload_token,
             $response->video_id,
-            $response->upload_server_uri
+            $response->upload_server_uri ?? null,
+            $response->endpoint ?? null,
+            $response->security_token ?? null,
+            $response->oss_bucket ?? null,
+            $response->oss_object ?? null,
+            $response->temp_access_id ?? null,
+            $response->temp_access_secret ?? null,
+            $response->expire_time ?? null
         );
     }
 
-    public function __construct(string $uploadToken, string $videoId, string $uploadServerUri)
-    {
+    public function __construct(
+        string $uploadToken,
+        string $videoId,
+        ?string $uploadServerUri = null,
+        ?string $endpoint = null,
+        ?string $securityToken = null,
+        ?string $ossBucket = null,
+        ?string $ossObject = null,
+        ?string $tempAccessId = null,
+        ?string $tempAccessSecret = null,
+        ?string $expireTime = null
+    ) {
         $this->uploadToken = $uploadToken;
         $this->videoId = $videoId;
         $this->uploadServerUri = $uploadServerUri;
+        $this->endpoint = $endpoint;
+        $this->securityToken = $securityToken;
+        $this->ossBucket = $ossBucket;
+        $this->ossObject = $ossObject;
+        $this->tempAccessId = $tempAccessId;
+        $this->tempAccessSecret = $tempAccessSecret;
+        $this->expireTime = $expireTime;
     }
 
     /**
@@ -72,9 +131,65 @@ class Create extends BaseResponse implements JsonResponse
     }
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getUploadServerUri(): string
+    public function getEndpoint(): ?string
+    {
+        return $this->endpoint;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSecurityToken(): ?string
+    {
+        return $this->securityToken;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getOssBucket(): ?string
+    {
+        return $this->ossBucket;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getOssObject(): ?string
+    {
+        return $this->ossObject;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTempAccessId(): ?string
+    {
+        return $this->tempAccessId;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTempAccessSecret(): ?string
+    {
+        return $this->tempAccessSecret;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getExpireTime(): ?string
+    {
+        return $this->expireTime;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getUploadServerUri(): ?string
     {
         return $this->uploadServerUri;
     }
--- tests/Api/Response/CreateTest.php
+++ tests/Api/Response/CreateTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 
 class CreateTest extends TestCase
 {
-    public function testJson()
+    public function testJsonInOriginalMethod()
     {
         $create = Create::json(json_encode([
             'upload_token' => '1a2b3c4d',
@@ -21,6 +21,32 @@ class CreateTest extends TestCase
         $this->assertSame('g01.upload.youku.com', $create->getUploadServerUri());
     }
 
+    public function testJsonInOssMethod()
+    {
+        $create = Create::json(json_encode([
+            'upload_token' => '1a2b3c4d',
+            'video_id' => 'fake-video-id',
+            'endpoint' => 'oss-cn-shanghai.aliyuncs.com',
+            'security_token' => 'fake-security-token',
+            'oss_bucket' => 'yk-source-upload',
+            'oss_object' => 'fake-oss-object',
+            'temp_access_id' => 'fake-temp-access-id',
+            'temp_access_secret' => 'fake-temp-access-secret',
+            'expire_time' => '2019-01-07T13:03:04Z',
+        ]));
+
+        $this->assertInstanceOf(Create::class, $create);
+        $this->assertSame('1a2b3c4d', $create->getUploadToken());
+        $this->assertSame('fake-video-id', $create->getVideoId());
+        $this->assertSame('oss-cn-shanghai.aliyuncs.com', $create->getEndpoint());
+        $this->assertSame('fake-security-token', $create->getSecurityToken());
+        $this->assertSame('yk-source-upload', $create->getOssBucket());
+        $this->assertSame('fake-oss-object', $create->getOssObject());
+        $this->assertSame('fake-temp-access-id', $create->getTempAccessId());
+        $this->assertSame('fake-temp-access-secret', $create->getTempAccessSecret());
+        $this->assertSame('2019-01-07T13:03:04Z', $create->getExpireTime());
+    }
+
     public function testJsonBuildFailed()
     {
         $create = Create::json(json_encode([
@@ -30,7 +56,7 @@ class CreateTest extends TestCase
         $this->assertNull($create);
     }
 
-    public function testConstruct()
+    public function testConstructInOriginalMethod()
     {
         $create = new Create('1a2b3c4d', 'xxxxxx', 'g01.upload.youku.com');
 
@@ -38,5 +64,32 @@ class CreateTest extends TestCase
         $this->assertSame('1a2b3c4d', $create->getUploadToken());
         $this->assertSame('xxxxxx', $create->getVideoId());
         $this->assertSame('g01.upload.youku.com', $create->getUploadServerUri());
+    }
+
+    public function testConstructInOssMethod()
+    {
+        $create = new Create(
+            '1a2b3c4d',
+            'fake-video-id',
+            null,
+            'oss-cn-shanghai.aliyuncs.com',
+            'fake-security-token',
+            'yk-source-upload',
+            'fake-oss-object',
+            'fake-temp-access-id',
+            'fake-temp-access-secret',
+            '2019-01-07T13:03:04Z'
+        );
+
+        $this->assertInstanceOf(Create::class, $create);
+        $this->assertSame('1a2b3c4d', $create->getUploadToken());
+        $this->assertSame('fake-video-id', $create->getVideoId());
+        $this->assertSame('oss-cn-shanghai.aliyuncs.com', $create->getEndpoint());
+        $this->assertSame('fake-security-token', $create->getSecurityToken());
+        $this->assertSame('yk-source-upload', $create->getOssBucket());
+        $this->assertSame('fake-oss-object', $create->getOssObject());
+        $this->assertSame('fake-temp-access-id', $create->getTempAccessId());
+        $this->assertSame('fake-temp-access-secret', $create->getTempAccessSecret());
+        $this->assertSame('2019-01-07T13:03:04Z', $create->getExpireTime());
     }
 }
